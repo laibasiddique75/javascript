34035b55ec7d36c5d43018842e32c468ea9dc213
laibasiddique75 <laibasiddique111@gmail.com>
2024-04-20 19:48:22 +0500

    done

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+    // Define a function to greet the visitor
+    function greetVisitor() {
+        // Prompt the visitor for their name
+        var name = prompt("Hello! What's your name?");
+        
+        // Check if the visitor entered a name
+        if (name !== null && name !== "") {
+            // Display a greeting message with the visitor's name
+            alert("Hello, " + name + "! Welcome to our website!");
+        } else {
+            // If the visitor didn't enter a name, display a generic greeting
+            alert("Hello! Welcome to our website!");
+        }
+    }
+
+    // Call the greetVisitor function when the page loads
+    window.onload = greetVisitor;
+
+
+// alert("Error! please enter a valid password");
+
+// alert("Welcome to JS Land...\n Happy Coding!");
+
+//  alert("Welcome to JS Land...");
+
+// alert(" Happy Coding! \n prevent this page from creating additional dialogs");
+
+// var message =  "Hello... I can run JS through my web browser's console"
+// console.log(message);
+ 
+
+//  alert("Hii connections");
