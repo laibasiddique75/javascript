25c34876063a7914d2c1a99df1bbff75f74c4e98
laibasiddique75 <laibasiddique111@gmail.com>
2024-08-01 20:09:16 +0500

    added

---
+++ chap43-48/app.js
@@ -0,0 +1,34 @@
+// Q no 1
+
+// function greet(){
+//     alert('Hello User')
+// }
+
+//  Q no 2
+// function thanks(){
+//     alert('Thanks purchasing phone from us')
+// }
+
+// Q no 3
+
+// function remove(e){
+//     e.parentNode.parentNode.remove()
+
+// }
+
+
+
+//  Q no 5
+
+// var counter = 0;
+
+// function increase(){
+//     document.getElementById("counter").innerText = counter++
+// }
+
+// function decrease(){
+//     if(counter<0){
+// counter = 0
+//     }
+//     document.getElementById("counter").innerText = counter--
+// }
